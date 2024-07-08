e06964ec3fdf8e0ceaebbf281ae6a2a9bc84cb3b
mhmooud nassar <mhmooud456@gmail.com>
2024-07-08 11:26:50 +0300

    Solved Reverse Words

---
+++ challenges-and-data-structures/Challenges/Reverse-Words/ReverseWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenges_and_data_structures.Challenges.Reverse_Words
+{
+    public static class ReverseWord
+    {
+        public static string ReverseWords(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            var words = input.Split(' ');
+            Array.Reverse(words);
+            return string.Join(" ", words);
+        }
+    }
+}
